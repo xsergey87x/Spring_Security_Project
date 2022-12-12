ddea670a230b1e5e59329c7abf33073fe051896d
Tytyanchuk Sergey <xsergey87x@ukr.net>
2022-12-12 22:47:32 +0200

    c1

---
+++ cursor/src/main/java/com/cursor/cursor/restController/AppController.java
@@ -0,0 +1,18 @@
+package com.cursor.cursor.restController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(value = "/")
+public class AppController {
+
+    @GetMapping(value = "/hello")
+    public String sayHello()
+    {
+        return "Hello";
+    }
+
+}
---
+++ cursor/src/test/java/com/cursor/cursor/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursor.cursor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
