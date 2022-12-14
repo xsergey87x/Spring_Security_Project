75f10d87088b470ba8ce315ddc501fcf377d8007
Tytyanchuk Sergey <xsergey87x@ukr.net>
2022-12-14 22:00:57 +0200

    c6

--- cursor/src/main/java/com/cursor/cursor/restController/AuthController.java
+++ cursor/src/main/java/com/cursor/cursor/restController/AuthController.java
@@ -2,11 +2,14 @@ package com.cursor.cursor.restController;
 
 
 import com.cursor.cursor.security.AuthenticationRequest;
+import com.cursor.cursor.security.JwtTokenUtil;
+import com.cursor.cursor.security.JwtUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +21,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthenticationManager authenticationManager;
+    private final JwtUserDetailsService jwtUserDetailsService;
+    private final JwtTokenUtil jwtTokenUtil;
 
     @PostMapping
     public ResponseEntity<String> authenticate(@RequestBody AuthenticationRequest request)
     {
             authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(),request.getPassword()));
+        UserDetails user = jwtUserDetailsService.loadUserByUsername(request.getEmail());
+        if (user != null)
+        {
+            return ResponseEntity.ok(jwtTokenUtil.generateToken(user));
+        }
+        return ResponseEntity
     }
 
 }
