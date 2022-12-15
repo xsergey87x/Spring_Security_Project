68dd4290a2aaa49af225bc452da2a90c0e3dc0a1
Tytyanchuk Sergey <xsergey87x@ukr.net>
2022-12-15 23:21:11 +0200

    c11

--- cursor/src/main/java/com/cursor/cursor/model/UserEntity.java
+++ cursor/src/main/java/com/cursor/cursor/model/UserEntity.java
@@ -1,11 +1,17 @@
 package com.cursor.cursor.model;
 
-//@Entity
-    public class UserEntity {
+import lombok.Data;
 
-//        @Id
-//        @GeneratedValue(strategy = GenerationType.AUTO)
-//        private Long id;
+import javax.persistence.*;
+
+@Data
+@Entity
+ public class UserEntity {
+
+    public UserEntity(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
 
     public UserEntity(String userName, String email, String password, String roles) {
         this.userName = userName;
@@ -14,10 +20,22 @@ package com.cursor.cursor.model;
         this.roles = roles;
     }
 
+    public UserEntity(String userName, String email, String password, boolean isActive, String roles) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.isActive = isActive;
+        this.roles = roles;
+    }
+
+            @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        private Long id;
+
         private String userName;
         private String email;
         private String password;
-
+        private boolean isActive;
         private String roles;
 
 }
---
+++ cursor/src/main/java/com/cursor/cursor/securityModel/CustomUserDetail.java
@@ -0,0 +1,62 @@
+package com.cursor.cursor.securityModel;
+
+import com.cursor.cursor.model.UserEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Component
+@RequiredArgsConstructor
+public class CustomUserDetail implements UserDetails {
+
+    private final String userName;
+    private final String password;
+    private final boolean isActive;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isActive;
+    }
+
+    public static UserDetails getUserDetailsFromUser(UserEntity user) {
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUserName(), user.getPassword(), user.isActive(), true,true,true,
+                new ArrayList<SimpleGrantedAuthority>());
+    }
+}
