c4cd13620bcbf550cca99443ba266e2c8228d80e
Tytyanchuk Sergey <xsergey87x@ukr.net>
2022-12-15 23:40:33 +0200

    c12

--- cursor/src/main/java/com/cursor/cursor/repository/UserRepository.java
+++ cursor/src/main/java/com/cursor/cursor/repository/UserRepository.java
@@ -1,8 +1,13 @@
 package com.cursor.cursor.repository;
 
+import com.cursor.cursor.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository {
+
+    Optional<UserEntity> findByUserName(String userName);
 }
--- cursor/src/main/java/com/cursor/cursor/security/JwtUserDetailsService.java
+++ cursor/src/main/java/com/cursor/cursor/security/JwtUserDetailsService.java
@@ -1,6 +1,8 @@
 package com.cursor.cursor.security;
 
 import com.cursor.cursor.model.UserEntity;
+import com.cursor.cursor.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,25 +10,30 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
+import javax.transaction.Transactional;
 import java.util.Collections;
 
+@RequiredArgsConstructor
 @Component
 public class JwtUserDetailsService implements UserDetailsService {
+
+    UserRepository userRepository;
+
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
 
         UserEntity user1 = new UserEntity("Artur","xxx@ukr.net","qwerty", "ROLE_ADMIN");
         UserEntity user2 = new UserEntity("John","yyy@ukr.net","ytrewq", "ROLE_USER");
 
-        if ("Artur".equals(userName))
-        {
+        UserEntity user = userRepository.findByUserName(userName).orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + userName));
+
+
+        if ("Artur".equals(userName)){
             return new User("Artur","qwerty", Collections.singleton(new SimpleGrantedAuthority("ROLE_ADMIN")));
-        }else if ("John".equals(userName))
-        {
+        }else if ("John".equals(userName)) {
             return new User("John","ytrewq", Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")));
-        }
- else {
-            throw new UsernameNotFoundException("User not found with username: " + userName);
-        }
+        } else {throw new UsernameNotFoundException("User not found with username: " + userName); }
+
     }
 }
